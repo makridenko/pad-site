b7a3ab33c58755517d51e91018115347b8a320fb
Alex M <makridenko.a@yandex.com>
2020-09-03 16:10:45 +0300

    done navbar component;

---
+++ client/src/Components/Navbar/Logo.js
@@ -0,0 +1,31 @@
+/* React */
+import React from 'react';
+
+/* Style */
+import styled from 'styled-components';
+
+/* Styled components */
+const LogoContainer = styled.div`
+  /* Position */
+  padding-top: 18px;
+  padding-bottom: 18px;
+  padding-left: 121px;
+  padding-right: 316px;
+`;
+
+const StyledLogo = styled.img`
+  /* Dimensions */
+  width: 90px;
+  height: 28px;
+`;
+
+
+const Logo = ({logoSrc}) => {
+  return (
+      <LogoContainer>
+        <StyledLogo src={logoSrc} />
+      </LogoContainer>
+  );
+};
+
+export default Logo;
