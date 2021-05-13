fa5655b64140ecd057914a3252a561165e98cbf5
Alex M <makridenko.a@yandex.ru>
2021-05-13 18:24:47 +0300

    add members app

--- kernel/config/settings.py
+++ kernel/config/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'events',
     'music',
     'bio',
+    'members',
 ]
 
 MIDDLEWARE = [
---
+++ kernel/members/models/member.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+
+class Member(models.Model):
+    """
+    Model for member tabel in database.
+    Member - info about member of the band.
+    """
+
+    first_name = models.CharField(
+        max_length=255, verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=255, verbose_name='Фамилия',
+    )
+    is_contact_face = models.BooleanField(
+        default=False, verbose_name='Контактное лицо',
+    )
+    vk_link = models.URLField(
+        blank=True, null=True, verbose_name='Ссылка на vk.com',
+    )
+    telegram_link = models.URLField(
+        blank=True, null=True, verbose_name='Ссылка на телеграмм',
+    )
+    position = models.CharField(
+        max_length=255, verbose_name='Роль в группе',
+    )
+    mail = models.EmailField(blank=True, null=True, verbose_name='Email')
+
+    class Meta:
+        verbose_name = 'Участник'
+        verbose_name_plural = 'Участники'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
