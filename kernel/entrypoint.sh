1b1f0e53e84462879655dba2d92d1c4e1f83fae1
Alex M <makridenko.a@yandex.ru>
2021-05-15 22:17:26 +0300

    bugfix

--- kernel/entrypoint.sh
+++ kernel/entrypoint.sh
@@ -3,4 +3,4 @@
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-gunicorn kernel.wsgi:application --bind 0.0.0.0:8000
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
